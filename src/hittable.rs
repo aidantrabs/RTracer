758ade36bed41c4d607b7af59684311683620576
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-15 03:18:19 -0500

    Update hittable object

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,17 +1,33 @@
-pub struct HitRecord {
-     pub p: Vec3,
-     pub normal: Vec3,
-     pub t: f32,
-     pub front_face: bool,
-}
+use crate::ray::Ray;
+use crate::vec3::Vec3;
 
-pub trait Hittable {
-     fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
+pub struct HitRecord {
+    pub p: Vec3,
+    pub normal: Vec3,
+    pub t: f32,
+    pub front_face: bool,
 }
 
 impl HitRecord {
-     pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
-          self.front_face = Vec3::dot(r.direction, outward_normal) < 0.0;
-          self.normal = if self.front_face { outward_normal } else { -outward_normal };
-     }
+    pub fn new() -> Self {
+        Self {
+            p: Vec3::new(0.0, 0.0, 0.0),
+            normal: Vec3::new(0.0, 0.0, 0.0),
+            t: 0.0,
+            front_face: false,
+        }
+    }
+
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
+        self.front_face = Vec3::dot(&r.direction, &outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
 }
