ec3eec763e3284adbbc3b36b1020a31c8deeb835
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-15 03:18:30 -0500

    Update hittable list object

--- src/hittable_list.rs
+++ src/hittable_list.rs
@@ -1,3 +1,6 @@
+use crate::hittable::*;
+use crate::ray::Ray;
+
 pub struct HittableList {
     pub objects: Vec<Box<dyn Hittable>>,
 }
@@ -7,27 +10,29 @@ impl HittableList {
         Self { objects: Vec::new() }
     }
 
-    pub fn clear(&mut self) {
-        self.objects.clear();
-    }
-
     pub fn add(&mut self, object: Box<dyn Hittable>) {
         self.objects.push(object);
     }
 
-     pub fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
-          let mut temp_rec = HitRecord::new();
-          let mut hit_anything = false;
-          let mut closest_so_far = t_max;
-     
-          for object in &self.objects {
-               if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
-                    hit_anything = true;
-                    closest_so_far = temp_rec.t;
-                    *rec = temp_rec;
-               }
-          }
-     
-          hit_anything
-     }
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::new();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+
+        hit_anything
+    }
 }
