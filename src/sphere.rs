fb7932022d2bd78836c59f6ef24724082192ba32
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-15 03:19:08 -0500

    Update sphere object

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,6 +1,6 @@
 use crate::vec3::Vec3;
-use crate::ray::Ray;
 use crate::hittable::*;
+use crate::ray::Ray;
 
 pub struct Sphere {
     pub center: Vec3,
@@ -8,25 +8,26 @@ pub struct Sphere {
 }
 
 impl Sphere {
-    pub fn new(center: Vec3, radius: f32) -> Sphere {
-        Sphere { center, radius }
+    pub fn new(center: Vec3, radius: f32) -> Self {
+        Self { center, radius }
     }
 }
 
 impl Hittable for Sphere {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
         let oc = r.origin - self.center;
-        let a = r.direction.length_squared();
-        let half_b = Vec3::dot(r.direction);
-        let c = oc.length_squared() - self.radius * self.radius;
-        
+        let a = Vec3::dot(&r.direction, &r.direction);
+        let half_b = Vec3::dot(&oc, &r.direction);
+        let c = Vec3::dot(&oc, &oc) - self.radius * self.radius;
         let discriminant = half_b * half_b - a * c;
+
         if discriminant < 0.0 {
             return false;
         }
 
         let sqrtd = discriminant.sqrt();
 
+        // Find the nearest root that lies in the acceptable range.
         let mut root = (-half_b - sqrtd) / a;
         if root < t_min || t_max < root {
             root = (-half_b + sqrtd) / a;
@@ -36,7 +37,7 @@ impl Hittable for Sphere {
         }
 
         rec.t = root;
-        rec.p = r.point_at_parameter(rec.t);
+        rec.p = r.at(rec.t);
         let outward_normal = (rec.p - self.center) / self.radius;
         rec.set_face_normal(r, outward_normal);
 
