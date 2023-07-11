d0b0453c1f1e76c680f1939efde0db98d563c0dd
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-07-11 07:21:41 -0400

    Refactor obj list

--- src/hittable_list.rs
+++ src/hittable_list.rs
@@ -1,28 +1,32 @@
-use crate::hittable::*;
-use crate::ray::Ray;
+use crate::hittable::{Hittable, HitRecord};
+use crate::Ray;
 
 pub struct HittableList {
-    list: Vec<Box<dyn Hittable>>,
+     objects: Vec<Box<dyn Hittable>>,
 }
 
 impl HittableList {
-    pub fn new(list: Vec<Box<dyn Hittable>>) -> Self {
-        Self { list }
-    }
+     pub fn new(list: Vec<Box<dyn Hittable>>) -> HittableList {
+          return HittableList { objects: list };
+     }
+
+     pub fn add(&mut self, object: Box<dyn Hittable>) {
+          self.objects.push(object);
+     }
 }
 
 impl Hittable for HittableList {
-    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
-        let mut hit_anything = None;
-        let mut closest_so_far = t_max;
+     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+          let mut hit_anything = None;
+          let mut closest_so_far = t_max;
 
-        for object in &self.list {
-            if let Some(hit_record) = object.hit(r, t_min, closest_so_far) {
-                closest_so_far = hit_record.t;
-                hit_anything = Some(hit_record);
-            }
-        }
+          for object in &self.objects {
+               if let Some(hit_record) = object.hit(r, t_min, closest_so_far) {
+                    closest_so_far = hit_record.t;
+                    hit_anything = Some(hit_record);
+               }
+          }
 
-        hit_anything
-    }
+          return hit_anything;
+     }
 }
