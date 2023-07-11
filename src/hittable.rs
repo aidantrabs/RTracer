9150fd8fbbffc6241131f03b30fbe7f3667adbfe
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-07-11 04:10:52 -0400

    Refactor redundant code

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,33 +1,25 @@
+use std::sync::Arc;
+use crate::Vec3;
+use crate::vec3::Point3;
 use crate::ray::Ray;
-use crate::vec3::Vec3;
+use crate::material::Material;
 
 pub struct HitRecord {
-    pub p: Vec3,
+    pub p: Point3,
     pub normal: Vec3,
+    pub material_ptr: Arc<dyn Material>,
     pub t: f32,
     pub front_face: bool,
 }
 
 impl HitRecord {
-    pub fn new() -> Self {
-        Self {
-            p: Vec3::new(0.0, 0.0, 0.0),
-            normal: Vec3::new(0.0, 0.0, 0.0),
-            t: 0.0,
-            front_face: false,
-        }
-    }
-
-    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
-        self.front_face = Vec3::dot(&r.direction, &outward_normal) < 0.0;
-        self.normal = if self.front_face {
-            outward_normal
-        } else {
-            -outward_normal
-        };
-    }
+     #[inline]
+     pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+          self.front_face = Vec3::dot(r.direction(), *outward_normal) < 0.0;
+          self.normal = if self.front_face { *outward_normal } else { -1.0 * (*outward_normal) };
+     }
 }
 
-pub trait Hittable {
-    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
+pub trait Hittable: Send + Sync {
+     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
 }
