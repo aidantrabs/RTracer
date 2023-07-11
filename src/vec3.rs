5292c26fcad4c9f207d10d670b504877286982c7
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-07-11 03:30:09 -0400

    Refactor vec3 struct

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,72 +1,139 @@
 use std::ops::{Add, Sub, Mul, Div, Neg};
-use rand::Rng;
+use crate::utils::{degrees_to_radians, random_f32, random_f32_range, clamp};
 
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+#[derive(Debug, Clone, Copy)]
 pub struct Vec3 {
      e: [f32; 3],
 }
 
 impl Vec3 {
      pub fn new(e0: f32, e1: f32, e2: f32) -> Vec3 {
-          Vec3 {
+          let vec = Vec3 {
                e: [e0, e1, e2]
           }
+
+          return vec;
      }
 
      pub fn x(&self) -> f32 {
-          self.e[0]
+          return self.e[0]
      }
 
      pub fn y(&self) -> f32 {
-          self.e[1]
+          return self.e[1]
      }
 
      pub fn z(&self) -> f32 {
-          self.e[2]
-     }
-     
-     pub fn dot(u: &Vec3, v: &Vec3) -> f32 {
-          u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
+          return self.e[2]
      }
 
-     pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
-          Vec3 {
-              e: [
-                    u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                    u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                    u.e[0] * v.e[1] - u.e[1] * v.e[0],
-              ] 
-          }
-     }
-
-     pub fn length(self) -> f32 {
-          (self.e[0] * self.e[0] + self.e[1] * self.e[1]+ self.e[2] * self.e[2]).sqrt()
+     pub fn length(&self) -> f32 {
+          return (self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]).sqrt() as f32;
      }
 
      pub fn length_squared(self) -> f32 {
-          self.e[0] * self.e[0] + self.e[1] * self.e[1]+ self.e[2] * self.e[2]
+          return self.length() * self.length();
+     }
+     
+     #[inline]
+     pub fn dot(self, v: Vec3) -> Vec3 {
+          return self.e[0] * v.e[0] + self.e[1] * v.e[1] + self.e[2] * v.e[2];
      }
 
-     pub fn unit_vector(u: &Vec3) -> Vec3 {
-          *u / u.length()
+     #[inline]
+     pub fn cross(self, v: Vec3) -> Vec3 {
+          return Vec3 {
+               e: [
+                    self.e[1] * v.e[2] - self.e[2] * v.e[1],
+                    self.e[2] * v.e[0] - self.e[0] * v.e[2],
+                    self.e[0] * v.e[1] - self.e[1] * v.e[0],
+               ] 
+          }
      }
 
+     #[inline]
+     pub fn unit_vector(v: Vec3) -> Vec3 {
+          return v / v.length();
+     }
+
+     #[inline]
+     pub fn random() -> Vec3 {
+          return Vec3 {
+               e: [
+                    random_f32(),
+                    random_f32(),
+                    random_f32(),
+               ]
+          }
+     }
+
+     #[inline]
+     pub fn random_range(min: f32, max: f32) -> Vec3 {
+          return Vec3 {
+               e: [
+                    random_f32_range(min, max),
+                    random_f32_range(min, max),
+                    random_f32_range(min, max),
+               ]
+          }
+     }
+
+     #[inline]
      pub fn random_in_unit_sphere() -> Vec3 {
           loop {
-               let p = Vec3::new(
-                    rand::random::<f32>(),
-                    rand::random::<f32>(),
-                    rand::random::<f32>(),
-               ) * 2.0 - Vec3::new(1.0, 1.0, 1.0);
-               if p.length_squared() < 1.0 {
-                    return p;
+               let p = Vec3::random_range(-1.0, 1.0);
+
+               if p.length_squared() >= 1.0 {
+                    continue;
                }
+
+               return p;
           }
+     }
+
+     pub fn random_unit_vector() -> Vec3 {
+          return Vec3::unit_vector(Vec3::random_in_unit_sphere());
+     }
+
+     pub fn random_in_unit_disk() -> Vec3 {
+          loop {
+               let p = Vec3::new(random_f32_range(-1.0, 1.0), random_f32_range(-1.0, 1.0), 0.0);
+
+               if p.length_squared() >= 1.0 {
+                    continue;
+               }
+
+               return p;
+          }
+     }
+
+     pub fn near_zero(&self) -> bool {
+          const S: f32 = (1 / 10_i32.pow(8)) as f32;
+
+          return (self.e[0].abs() < S) && (self.e[1].abs() < S) && (self.e[2].abs() < S);
+     }
+
+     pub fn change(&mut self, v: Vec3) {
+          self.e[0] = v.x();
+          self.e[1] = v.y();
+          self.e[2] = v.z();
+     }
+
+     pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
+          return (*v) - 2.0 * v.dot(*n) * (*n);
+     }
+
+     pub fn refract(self, n: Vec3, etai_over_etat: f32) -> Vec3 {
+          let cos_theta = Vec3::dot(-1.0 * self, n).min(1.0);
+          let r_out_parallel = etai_over_etat * (self + cos_theta * n);
+          let r_out_perp = -(1.0 - r_out_parallel.length_squared()).sqrt() * n;
+
+          return r_out_parallel + r_out_perp;
      }
 }
 
 impl Add for Vec3 {
-     type Output = Self;
+     type Output = Vec3;
 
      fn add(self, v: Vec3) -> Vec3 {
           Vec3 {
